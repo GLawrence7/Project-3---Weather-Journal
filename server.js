20f803daaca55c5b3a656256c2d7156cc90c31cf
GLawrence7 <georgelawrence0790@gmail.com>
2020-04-30 16:01:11 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+/* Middleware*/
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log('running on localhost: ' + port);
+};
+
+const weatherData = [];
+
+// Initialise all route with callback function
+app.get('/all', getData);
+
+// Callback function to cary out GET '/all'
+function sendData(req, res) {
+
+  res.send(weatherData);
+
+  console.log('GET')
+  console.log(weatherData)
+
+
+};
+
+
+// Post route for weather sendData
+app.post('/addWeatherData', addWeatherData);
+
+function addWeatherData(req, res) {
+  newEntry = {
+    weather: req.body.currentWeather,
+    date: req.body.date,
+    userResponse: req.body.userResponse
+  }
+
+  weatherData.push(newEntry)
+  res.send(weatherData)
+  console.log('POST')
+  console.log(weatherData)
+};
---
+++ website/app.js
@@ -0,0 +1,89 @@
+/* Global Variables */
+const apiKey = '&appid=6f2b809108a9e9accceb41304658d42f'
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener adds function to DOM
+document.getElementById('generate').addEventListener('click', performAction);
+
+// Function called by event listener
+function performAction(e) {
+  const myCity = document.getElementById('city').value;
+  const myFeelings = document.getElementById('feelings').value;
+
+  getWeatherData(baseURL, myCity, apiKey).then(function(weatherData) {
+    postData('http://localhost:3000/addWeatherData', {
+      date: newDate,
+      currentWeather: weatherData.weather[0].main,
+      userResponse: myFeelings
+    })
+  }).then(function() {
+    updateUI('http://localhost:3000/all')
+  }).then(function(value) {
+    console.log('all promises resolved');
+  }, function(reason) {
+    console.log('error in promises');
+  })
+}
+
+// Function to GET Web API data
+const getWeatherData = async (baseURL, myCity, apiKey) => {
+
+  console.log('WEATHER');
+
+  const res = await fetch(baseURL + myCity + apiKey)
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+// Function to POST data
+const postData = async (url = '', data = {}) => {
+
+  console.log('POST');
+  console.log(data);
+
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  console.log(data)
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// Function to GET Project Data and update UI
+const updateUI = async (url = '') => {
+
+  console.log('UPDATE');
+
+  const res = await fetch(url);
+  try {
+    const data = await res.json();
+    console.log(data)
+
+    document.getElementById("weather").innerHTML = 'Current Weather: ' + data.currentWeather;
+    document.getElementById("date").innerHTML = 'Date: ' + newDate;
+    document.getElementById("content").innerHTML = 'How I\'m feeling: ' + data.userResponse;
+
+} catch (error) {
+    console.log("error", error);
+  }
+}
